aab743e48dd10272924dc4ccd82ccc80f583dca2
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2025-04-25 04:34:48 +0900

    add black formatter

--- numbers-and-counters.py
+++ numbers-and-counters.py
@@ -1,68 +1,101 @@
 import pandas as pd
 import numpy as np
+import pykakasi
+
+kks = pykakasi.kakasi()
 
 np.random.seed(20250424)
 
 
 cfg = [
-    ("", {  # no suffix, i.e. create plain numbers to read
-          "n_random_cards": 20,
-          "min_value": 100,
-          "max_value": 1_000
-          }
+    (
+        "",
+        {  # no suffix, i.e. create plain numbers to read
+            "n_random_cards": 20,
+            "min_value": 100,
+            "max_value": 1_000,
+            "tags": "counters::plain",
+        },
     ),
-    ("", {  # Create 10 more cards for large numbers
-          "n_random_cards": 10,
-          "min_value": 1_000,
-          "max_value": 1_000_000
-          }
+    (
+        "",
+        {  # Create 10 more cards for large numbers
+            "n_random_cards": 10,
+            "min_value": 1_000,
+            "max_value": 1_000_000,
+            "tags": "counters::plain",
+        },
     ),
-    ("つ", {
-          "must_include": [1, 9],
-          }
-     ),
-    ("個", {
-          "must_include": [1, 10],
-          "n_random_cards": 10,
-          "min_value": 11,
-          "max_value": 100,
-          },
+    (
+        "つ",
+        {
+            "must_include": [1, 9],
+            "tags": "counters::tsu",
+        },
     ),
-    ("円", {
-          "n_random_cards": 30,
-          "min_value": 100,
-          "max_value": 10_000,
-          }
+    (
+        "個",
+        {
+            "must_include": [1, 10],
+            "n_random_cards": 10,
+            "min_value": 11,
+            "max_value": 100,
+            "tags": "counters::small-things",
+        },
     ),
-    ("ページ", {
-          "n_random_cards": 20,
-          "min_value": 10,
-          "max_value": 200,
-          }
+    (
+        "円",
+        {
+            "n_random_cards": 30,
+            "min_value": 100,
+            "max_value": 10_000,
+            "tags": "counters::yen",
+        },
+    ),
+    (
+        "ページ",
+        {
+            "n_random_cards": 20,
+            "min_value": 10,
+            "max_value": 200,
+            "tags": "counters::pages",
+        },
     ),
 ]
 
 result = []
 
 for counter, counter_cfg in cfg:
+    this_result = []
+
+    # Create the numbers themselves
     if "must_include" in counter_cfg:
-        result = result + [
-            str(i) + counter 
+        this_result = this_result + [
+            str(i) + counter
             for i in range(
-                counter_cfg["must_include"][0],
-                counter_cfg["must_include"][1] + 1
+                counter_cfg["must_include"][0], counter_cfg["must_include"][1] + 1
             )
         ]
     if "n_random_cards" in counter_cfg:
-        random_ints = sorted(np.random.randint(counter_cfg["min_value"], counter_cfg["max_value"], size=counter_cfg["n_random_cards"]))
-        result = result + [
-            str(i) + counter
-            for i in random_ints
-        ]
-    
+        random_ints = sorted(
+            np.random.randint(
+                counter_cfg["min_value"],
+                counter_cfg["max_value"],
+                size=counter_cfg["n_random_cards"],
+            )
+        )
+        this_result = this_result + [str(i) + counter for i in random_ints]
+
+    # Add tags, furigana, etc.
+    this_result = [
+        number + "," + kks.convert("number")["hira"] + "," + counter_cfg["tags"]
+        for number in this_result
+    ]
+
+    result = result + this_result
+
 
 # print(result)
 
-result_df = pd.DataFrame(result, columns=["number"])
+result_df = pd.DataFrame(result)
 result_df.to_csv("japanese-numbers.csv", index=None, header=False)
-
